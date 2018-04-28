571ee2f867cd37a860f16c93342de3121cf5c148
Miha Pleško <miha.plesko@xlab.si>
2018-04-28 21:27:52 +0200

    MVP working
    With this commit we implement enough logic to make
    program already usable. Input directory and output
    directory are still hard-coded, however.
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- izbirnik.py
+++ izbirnik.py
@@ -1,18 +1,23 @@
+import os
+import re
 import tkinter
+import shutil
 
 
 class UI(tkinter.Frame):
     status_text = None
 
-    def __init__(self):
+    def __init__(self, backend=None):
         self.master = tkinter.Tk()
         self.master.geometry('750x500+100+100')
         self.master.title('Izbirnik')
+        self.backend = backend
         super().__init__(self.master, bg='white', borderwidth=10)
         self.pack(expand=True, fill=tkinter.BOTH)
         self.create_widgets()
 
-        self.files_found = []
+        self.matched_files = []
+        self.log('Vnesite ime datoteke, nato pritisnite ENTER.')
 
     def create_widgets(self):
         self.title = tkinter.Label(self, text='Izbirnik', background='white', borderwidth=0, font=("Courier", 44))
@@ -23,40 +28,79 @@ class UI(tkinter.Frame):
         self.pattern.bind('<Return>', self.on_pattern_input)
         self.pattern.focus()
 
-        self.files_found_container =
-
-        # self.hi_there = tkinter.Button(self)
-        # self.hi_there["text"] = "Hello World\n(click me)"
-        # self.hi_there["command"] = self.say_hi
-        # self.hi_there.pack()
-
-        # self.quit = tkinter.Button(self, text="Zapri", fg="red", command=self.master.destroy)
-        # self.quit.pack()
+        self.files_found_container = tkinter.Frame(self, width=200, height=200, background='white')
+        self.files_found_container.pack(fill=tkinter.X)
 
         self.status_text = tkinter.StringVar()
         self.status = tkinter.Label(self, textvariable=self.status_text, background='white', borderwidth=0, font=("Courier", 20))
         self.status.pack(side=tkinter.BOTTOM)
 
     def on_pattern_input(self, *args, **kwargs):
-        self.say_hi()
+        pattern = self.pattern.get()
+        self.matched_files = self.backend.match_pattern(pattern)
+        self.show_buttons()
 
-    def add_buttons(self):
-        file_found = tkinter.Button(self, )
-        self.files_found.append(file_found)
+    def on_button_click(self, btn, file_found):
+        self.backend.copy_file(file_found)
+        btn.config(state=tkinter.DISABLED, )
+        self.log('Skopirano.')
 
+    def show_buttons(self):
+        self.clear_buttons()
 
-    def say_hi(self):
-        print("hi there, everyone!")
-        self.status_text.set('BUREK')
+        if self.matched_files:
+            tkinter.Label(self.files_found_container, text='Zadetki:', font=("Courier", 20), background='white', pady=10).pack()
+            self.log('Kliknite puljubni zadetek da ga skopirate.')
+        else:
+            self.log('Ni zadetkov.')
+
+        for f in self.matched_files:
+            btn = tkinter.Button(
+                self.files_found_container,
+                text=f.path,
+                font=("Courier", 10),
+                pady=10,
+                background='white',
+            )
+            btn.config(command=lambda: self.on_button_click(btn, f))
+            btn.pack(fill=tkinter.X)
+
+    def clear_buttons(self):
+        for b in self.files_found_container.winfo_children():
+            b.pack_forget()
+
+    def log(self, txt):
+        self.status.config(foreground='#cccccc')
+        self.status_text.set(txt)
 
 
 class Program:
     def __init__(self):
         self.ui = None
+        self.input_dir = r'C:\Users\Miha\PycharmProjects\izbirnik\example\input'
+        self.output_dir = r'C:\Users\Miha\PycharmProjects\izbirnik\example\output'
 
     def start_gui(self):
-        self.ui = UI()
+        self.ui = UI(backend=self)
         self.ui.mainloop()
+
+    def match_pattern(self, pattern):
+        r = re.compile('^{}(\..+)?$'.format(pattern))
+        res = []
+        for subdir, dirs, files in os.walk(self.input_dir):
+            for filename in files:
+                if r.match(filename):
+                    res.append(MatchedFile(os.path.join(subdir, filename)))
+        return res
+
+    def copy_file(self, matched_file):
+        print('Copying {} into {}'.format(matched_file.path, self.output_dir))
+        shutil.copy(matched_file.path, self.output_dir)
+
+
+class MatchedFile:
+    def __init__(self, path):
+        self.path = path
 
 
 p = Program()
